4b4c0886708d278a3dc5dd4f753f8cee65e7be78
LucasNotaro <lucas.sn@live.com>
2023-10-23 22:29:31 -0300

    tuplas

---
+++ AULA 10/exercicio1-refeito.py
@@ -0,0 +1,29 @@
+# Faça um algoritmo que carregue uma tupla de 10 elementos numéricos inteiros.
+# Após a finalização da entrada, o algoritmo deve escrever a mesma tupla, na ordem
+# inversa de entrada.
+
+# Inicializa a variável 'i' com 0. Esta variável será usada como contador.
+i = 0
+
+# Inicializa uma lista vazia chamada 'lista'. Esta lista armazenará os números digitados pelo usuário.
+lista = []
+
+# Este é um loop while que continuará até que 'i' seja menor que 10.
+while i < 10:
+    # Solicita ao usuário para digitar um número. O número digitado é convertido para um inteiro e armazenado na variável 'x'.
+    x = int(input("Digíte um número: "))
+
+    # O número digitado pelo usuário é adicionado ao final da lista.
+    lista.append(x)
+
+    # Incrementa o contador 'i' em 1.
+    i = i + 1
+
+# Converte a lista de números em uma tupla e armazena na variável 't'.
+t = tuple(lista)
+
+# Imprime a tupla 't'.
+print(t)
+
+# Imprime a tupla 't' em ordem inversa.
+print(t[::-1])
